06e0b14bc3fa56aa9d09635973ad4a27b5c90fa7
Umut Murat Ayaz <umutmuratayaz@gmail.com>
2022-06-13 22:46:12 +0300

    Updated

--- Odev12.sql
+++ Odev12.sql
@@ -16,12 +16,9 @@ WHERE rental_rate =
 
 
 -- 3. film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
-SELECT title,rental_rate,replacement_cost FROM film
-WHERE rental_rate =
-(SELECT MIN(rental_rate) FROM film)
+SELECT title,rental_rate,replacement_cost FROM film WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
 AND 
-replacement_cost =
-(SELECT MIN(replacement_cost) FROM film)
+replacement_cost = (SELECT MIN(replacement_cost) FROM film)
 
 
 -- 4. payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
